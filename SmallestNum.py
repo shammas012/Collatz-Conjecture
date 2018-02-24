b968267fca97104f03d035f0566281491f1f988d
shammas012 <36211745+shammas012@users.noreply.github.com>
2018-02-24 14:06:38 +0000

    Week 5 exercise added
    The exercise was to create a python program that finds the smallest integer value that is divisible by all numbers between and including 1 and 20.

---
+++ SmallestNum.py
@@ -0,0 +1,19 @@
+# Author: Mohamed Shammas Mohamed Ali Manaf 
+# Date : 2018-02-24
+# Description : Program that calculates the smallest number divisible evenly by all numbers between 0 and 20
+# Note : The program took almost 3 minutes to display the o/p because of the volume of loops involved.
+
+smallestNum = 0 										# variable to assign the smallest number
+intValue = 0											# variable for iterating over the integers; starting at zero
+while smallestNum == 0: 								# this while loop will iterate through all integers until the smallest evenly divisble number is found
+	#print(i)
+	for divisor in range(1,21):							# the range returns the list of integers from 1 to 20
+		if intValue % divisor == 0: 					# check if number contained in the current "intValue" is evenly divisble by the "divisor"
+			if divisor == 20 and intValue % divisor == 0: # assume if the divisor is 20, at this stage the "intValue" should be divisible by all integers preceeding the "divisor" in numerical order.
+				smallestNum = intValue					  # assign the current value in intValue to smallestNum
+				break;
+		else:
+			break;										# break the looping if the value in intValue is not evenly divisble by any of the elements in the range list.
+	intValue+=1											# increment the value of intValue by 1
+print('The smallest number evenly divisible by all number bewteen and incluing 1 and 20 is : ',smallestNum)										# Display value
+		
